3161390d29d27e5c634abeac42b37bd5c0e79bde
William Dueber <bill@dueber.com>
2012-03-30 15:26:06 -0400

    Removed keepalive

--- sys/DBH.php
+++ sys/DBH.php
@@ -19,8 +19,9 @@ class DBH {
      
      try {
        self::$dbh = new PDO("mysql:host=$host;dbname=$db", $uname, $pass, array(
-           PDO::ATTR_PERSISTENT => true
+           // PDO::ATTR_PERSISTENT => true
        )); 
+       self::$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      } catch (PDOException $e) {
        print "Error!: " . $e->getMessage() . "<br/> $host / $db / $uname/ $pass";
        die();

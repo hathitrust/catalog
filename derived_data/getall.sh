e5549c0bd3f335aecbb20395e840917d3f971bfb
William Dueber <bill@dueber.com>
2012-04-02 10:27:13 -0400

    Added getall.sh to get derived data

---
+++ derived_data/getall.sh
@@ -0,0 +1,13 @@
+HOST=`hostname | perl -ne '@h = split(/[-.]/); print $h[0]'`
+
+if [ $HOST == 'koolaid' ]
+then
+  DIR=/htapps/dueberb.catalog/web/derived_data
+else
+  DIR=/htapps/catalog/web/derived_data
+fi
+
+
+perl $DIR/gfv.pl language > $DIR/language.txt
+perl $DIR/gfv.pl format > $DIR/format.txt
+php -f  $DIR/getHTNamespaceMap.php > $DIR/ht_namespaces.php

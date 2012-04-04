e7ad6109e914e5cc1fc90df6ab497d47e1c8cbee
William Dueber <bill@dueber.com>
2012-04-04 12:44:44 -0400

    Added capistrano stuff

---
+++ config/deploy.rb
@@ -0,0 +1,75 @@
+set :stages, %w(production staging)
+set :default_stage, "staging"
+require 'capistrano/ext/multistage'
+
+
+set :application, "catalog"
+
+ssh_options[:forward_agent] = true
+
+set :scm,         :git
+set :repository,  "/htapps/dueberb.catalog/ht"
+
+
+set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules", "interface/compile"]
+
+# Call the current dir "web"
+set :current_dir, "web"
+
+
+# Do we need to mess with the schema?
+
+set :branch do
+  tags = `git tag`.split("\n")
+  default_tag = tags.last
+  puts "\n\nTags:\n  " + tags.join("\n  ");
+  
+  tag = Capistrano::CLI.ui.ask "\n Tag to deploy (make sure to push the tag first): [#{default_tag}] "
+  tag = default_tag if tag.empty?
+  tag
+end
+
+namespace :vf do
+
+  task :mkcompile do
+    compileDir = "#{deploy_to}/web/interface/compile"
+    run "mkdir -p #{compileDir}"
+    run "chmod 777 #{compileDir}"
+  end
+
+  task :mkreleases do
+    run "mkdir -p #{deploy_to}/releases"
+  end
+
+
+  task :mkDBTables do
+    migrate = nil
+    until ['Y', 'N'].include? migrate
+      migrate =  Capistrano::CLI.ui.ask "Do you need to run a db migration as Bill (Y/N) [N] "
+    end
+
+    if migrate == 'Y'
+      schemafile = "#{deploy_to}/web/mysql/schema.mysql"
+      password = Capistrano::CLI.ui.ask "Password: "
+      run "mysql -u dueberb -h mysql-sdr -p #{password} vufind < #{schemafile}"
+    end
+  end
+end
+
+before "deploy:update", "vf:mkreleases"
+after "deploy:create_symlink", "vf:mkDBTables"
+after "deploy:create_symlink", "vf:mkcompile"
+
+
+# Undefine stuff we don't use
+
+#namespace :deploy do
+#  task :start do
+#  end
+#  task :migrate do
+#  end
+#  task :stop do
+#  end
+#  task :restart do
+#  end
+#end
---
+++ config/deploy/production.rb
@@ -0,0 +1,6 @@
+role :app, "moxie-1"
+set :deploy_via, :copy
+set :deploy_to, "/htapps/catalog"
+
+
+

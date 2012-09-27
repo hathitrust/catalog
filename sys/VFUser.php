9460baa4e2829b4c00e9985fce2687bc7c88bbf5
BillDueber <bill@dueber.com>
2012-09-27 11:51:11 -0400

    Always return NULL for VFUser until login is implemented

--- sys/VFUser.php
+++ sys/VFUser.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once 'CatalogConnection.php';
 require_once 'sys/ActivityLog.php';
 /*
   Create a specialized user class, since the ones that came with VUFind are a little scattered.
@@ -39,62 +38,63 @@ class VFUser
   
   public function singleton()
   {
-    global $configArray;    
-    $sess = VFSession::singleton();
-    
-    // Get the user from (a) the instance, (b) the session, or (c) the constructor
-    // and make sure it ends up in self::$instance
-    
-    
-    $user = self::$instance;
-    
-    if (!isset($user)) {
-      if ($sess->is_set('user')) {
-        $user = $sess->get('user');                
-        // echo "Got from session<br>";
-      } else {
-        $c = __CLASS__;
-        $user = new $c();
-        $sess->set('user', $user);
-        // echo "Created new<br>";
-      } 
-      self::$instance = $user;
-    } 
-    
-    // If we've got a patron, we're already logged in
-    
-    if (isset($user, $user->patron)) {
-      // echo "Already logged in<br>";
-      return $user;
-    }
-
-    // If we don't have a uniqname, we're not logged in
-    
-    if (!$sess->is_set('uniqname')) {
-      // echo "Not logged in<br>";
       return NULL;
-    }
-    
-    // Otherwise, we're logged in and need patron data.
-    
-    // echo "Getting login information<br>";
-    $uq = $sess->get('uniqname');
-    $catalog = new CatalogConnection($configArray['Catalog']['driver']);      
-    $user->username = $uq;
-    $user->password = $uq;
-    $user->cat_username = $uq;
-    $user->cat_password = $uq;
-    $user->ip = $sess->get('ip');
-    $user->patron = $catalog->patronLogin($uq, $uq);
-      
-    $alog = ActivityLog::singleton();
-    if (isset($user->patron)) {
-      $alog->log('login', $uq, $user->patron->campus, $user->patron->bor_status);
-    } else {
-      $alog->log('login', $uq, 'NPR');
-    }
-
-    return $user;
+//    global $configArray;
+//    $sess = VFSession::singleton();
+//
+//    // Get the user from (a) the instance, (b) the session, or (c) the constructor
+//    // and make sure it ends up in self::$instance
+//
+//
+//    $user = self::$instance;
+//
+//    if (!isset($user)) {
+//      if ($sess->is_set('user')) {
+//        $user = $sess->get('user');
+//        // echo "Got from session<br>";
+//      } else {
+//        $c = __CLASS__;
+//        $user = new $c();
+//        $sess->set('user', $user);
+//        // echo "Created new<br>";
+//      }
+//      self::$instance = $user;
+//    }
+//
+//    // If we've got a patron, we're already logged in
+//
+//    if (isset($user, $user->patron)) {
+//      // echo "Already logged in<br>";
+//      return $user;
+//    }
+//
+//    // If we don't have a uniqname, we're not logged in
+//
+//    if (!$sess->is_set('uniqname')) {
+//      // echo "Not logged in<br>";
+//      return NULL;
+//    }
+//
+//    // Otherwise, we're logged in and need patron data.
+//
+//    // echo "Getting login information<br>";
+//    $uq = $sess->get('uniqname');
+//    $catalog = new CatalogConnection($configArray['Catalog']['driver']);
+//    $user->username = $uq;
+//    $user->password = $uq;
+//    $user->cat_username = $uq;
+//    $user->cat_password = $uq;
+//    $user->ip = $sess->get('ip');
+//    $user->patron = $catalog->patronLogin($uq, $uq);
+//
+//    $alog = ActivityLog::singleton();
+//    if (isset($user->patron)) {
+//      $alog->log('login', $uq, $user->patron->campus, $user->patron->bor_status);
+//    } else {
+//      $alog->log('login', $uq, 'NPR');
+//    }
+//
+//    return $user;
   }
   
 }                                    

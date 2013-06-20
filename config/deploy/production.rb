947dae54665c550beae88d05c33fd4b69ec526f8
Bill Dueber <bill@dueber.com>
2013-06-20 09:11:39 -0400

    Deploy everywhere

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,7 @@
-role :app, "moxie-1", "lassi"
+# role :app, "moxie-1", "lassi" # Both
+role :app, "lassi"              # JUST ICTC
+role :app, "moxie-1"          # JUST MACC
+
 set :deploy_via, :copy
 set :deploy_to, "/htapps/catalog"
 

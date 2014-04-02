4533bdbc662d8cd5ee33fbed7a028822bc7995fe
Bill Dueber <bill@dueber.com>
2014-04-02 11:43:23 -0400

    Added simple rollback

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,59 @@ SERVERS="moxie-1 rootbeer-1"
 
 
 DATE=`date '+%Y%m%d%H%M_%S'`
-DEPLOYDIR="/htapps/catalog/releases/$DATE"
+DEPLOYROOT="/htapps/catalog/releases"
+DEPLOYDIR="$DEPLOYROOT/$DATE"
+
+
 SYMLINKDIR="/htapps/catalog/web"
+
 
 ### DEBUG ###
 #SERVERS="waffle"
 #DEPLOYDIR="/tmp/releases/$DATE"
 #SYMLINKDIR="/tmp/web"
-
+#SYMLINKDIR="/htapps/catalog/webtest"
 ##############
 
+
+function rollback() {
+  for i in $SERVERS; do
+    RECENT=`ssh $i ls -1rt $DEPLOYROOT | tail -2 | head -1`
+    echo "Most recent deploys"
+    echo
+    ssh $i ls -1rt $DEPLOYROOT 
+    echo
+    read -r -p "Roll back to when (default: $RECENT) "
+    if [[ -z $REPLY ]]; then
+      ROLLBACK=$RECENT;
+    else
+      ROLLBACK=$REPLY
+    fi;
+    ROLLBACKDIR="$DEPLOYROOT/$ROLLBACK"
+    echo "Choosing to roll back to $ROLLBACKDIR"
+    echo "Will link $SYMLINKDIR to $ROLLBACKDIR"
+    echo
+    echo
+    break
+  done
+  for server in $SERVERS; do
+    echo "Working on $server"
+    ssh $server <<EOF
+      rm -f $SYMLINKDIR
+      ln -s $ROLLBACKDIR $SYMLINKDIR;
+EOF
+  done;
+}
+
+
+if [[ $1 = 'rollback' ]]; then
+  rollback
+  exit
+fi
+
 echo
-echo Latest tag:
-git tags | tail -1
+echo Latest tags:
+git tags | tail -4
 TAG=`git describe --abbrev=0`
 echo
 echo 
@@ -31,7 +71,6 @@ else
   echo "Aborting..."
   exit
 fi
-
 
 
 function deploy() {

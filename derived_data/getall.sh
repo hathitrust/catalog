e134ca958b36ea7522d29ae848f3997d0032481c
Bill Dueber <bill@dueber.com>
2016-04-20 10:48:49 -0400

    Restored getall.sh

--- derived_data/getall.sh
+++ derived_data/getall.sh
@@ -2,6 +2,11 @@
 
 DIR=$1
 
+if [ -z $DIR ]; then
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
+
 perl $DIR/gfv.pl language > $DIR/language.txt
 perl $DIR/gfv.pl format > $DIR/format.txt
 php -f  $DIR/getHTNamespaceMap.php
+php -f  $DIR/getHTCollectionMap.php

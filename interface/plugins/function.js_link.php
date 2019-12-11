a52be2d9162a2ee1430d706245f1f72d1af4a65f
Bill Dueber <bill@dueber.com>
2019-12-11 15:56:56 -0500

    Added missing plugins from Roger Halloween update

---
+++ interface/plugins/function.js_link.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * File:     function.css_link.php
+ * Type:     function
+ * Name:     timestamp
+ * Purpose:  outputs a random magic answer
+ * -------------------------------------------------------------
+ */
+function smarty_function_js_link($params, &$smarty)
+{
+  $base_filename = preg_replace('#^/([^/]+)/#', '/${1}/web/', $params['href']);
+  $root = dirname($_SERVER['DOCUMENT_ROOT']);
+  $filename = FALSE;
+  if ( is_dir("$root/common") ) {
+    $filename = "$root/${base_filename}";
+  } elseif ( is_dir("$root/../babel/common") ) {
+    $filename = "$root/../babel/${base_filename}";
+  }
+  $modtime = ( $filename === FALSE ) ? time() : filemtime($filename);
+  return "<script type=\"text/javascript\" src=\"${params['href']}?_=${modtime}\"></script>";
+
+}
+?>

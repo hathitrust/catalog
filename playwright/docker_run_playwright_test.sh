90a461cc1c00bf3d1d7b51c9b99b2211a6a6ef95
Aaron Elkiss <aelkiss@umich.edu>
2025-06-26 17:40:08 -0400

    Get playwright tests working for firebird
    Some may not pass in github (by virtue of being slow?). All are passing
    locally. We can see.

--- playwright/docker_run_playwright_test.sh
+++ playwright/docker_run_playwright_test.sh
@@ -9,7 +9,7 @@ npm install
 run_playwright_test(){
     speed=$1
     echo "running ${speed} playwright tests"
-    npx -y playwright test --config=test/${speed}/playwright.config.ts test/${speed}
+    npx -y playwright test --config=test/${speed}/playwright.config.ts test/${speed} --trace on
 }
 
 npm config set update-notifier false
--- playwright/test/slow/search_results_facets.spec.js
+++ playwright/test/slow/search_results_facets.spec.js
@@ -1,5 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
+// TODO refactor duplication for narrow versions -- extract function with a
+// conditional for the 'Show Search Filters'?
+
 // Click on "Latin" language facet and verify fewer results, then remove facet and verify more results.
 // Verify Japanese language facet is hidden before, and visible after,
 // expanding the language facet list.
@@ -7,43 +10,49 @@ test('Facets (non-mobile/fullwidth)', async ({ page, isMobile }) => {
   test.skip(isMobile === true, 'immobile-only test');
   await page.goto('/Search/Home?lookfor=journal');
   // Filter on Latin to get fewer results
-  await page.getByRole('button', { name: /Latin/ }).click();
-  await expect(page.getByRole('heading', { name: /3 Catalog results/i })).toBeVisible();
+  await page.getByRole('button', { name: "Language" }).click();
+  await page.getByRole('link', { name: "Latin" }).click();
+  // TODO ideally should get the original # of results and assert this is fewer
+  await expect(page.getByRole('heading', { name: /3 results/i })).toBeVisible();
   // Facet is part of URL
   await expect(page).toHaveURL(/filter%5B%5D=language%3ALatin/);
   // Remove facet in active-filters-list to get full number of results
-  await page.getByRole('button', { name: 'Language: Latin Remove' }).click();
-  await expect(page.getByRole('heading', { name: /174 Catalog results/i })).toBeVisible();
+  await page.getByRole('link', { name: 'Remove filter Language: Latin' }).click();
+  await expect(page.getByRole('heading', { name: /174 results/i })).toBeVisible();
   // Japanese is down in the mud
-  await expect(page.getByRole('button', { name: /Japanese/ })).not.toBeVisible();
+  await page.getByRole('button', { name: "Language" }).click();
+  await expect(page.getByRole('link', { name: "Japanese" })).not.toBeVisible();
   // "Show all" to make all language facets visible
   await page.getByRole('button', { name: /Show all \d+ language filters/i }).click();
-  await expect(page.getByRole('button', { name: /Japanese/ })).toBeVisible();
+  await expect(page.getByRole('link', { name: "Japanese" })).toBeVisible();
 });
 
 // This is essentially the same test as above but with "Options/Filters" toggled when necessary.
 test('Facets (mobile/width-shrink)', async ({ page }) => {
-  page.setViewportSize({ width: 900, height: 1500 });
+  page.setViewportSize({ width: 800, height: 1500 });
   await page.goto('/Search/Home?lookfor=journal');
   // Expand filters
-  await page.getByRole('button', { name: /Options\/Filters \(2\)/ }).click();
+  await page.getByRole('button', { name: "Show Search Filters" }).click();
   // Filter on Latin to get fewer results
-  await page.getByRole('button', { name: /Latin/ }).click();
-  await expect(page.getByRole('heading', { name: /3 Catalog results/i })).toBeVisible();
+  await page.getByRole('button', { name: "Language" }).click();
+  await page.getByRole('link', { name: "Latin" }).click();
+  // TODO ideally should get the original # of results and assert this is fewer
+  await expect(page.getByRole('heading', { name: /3 results/i })).toBeVisible();
   // Expand filters (again)
-  await page.getByRole('button', { name: /Options\/Filters \(3\)/ }).click();
+  await page.getByRole('button', { name: "Show Search Filters" }).click();
   // Facet is part of URL
   await expect(page).toHaveURL(/filter%5B%5D=language%3ALatin/);
   // Remove facet in active-filters-list to get full number of results
-  await page.getByRole('button', { name: 'Language: Latin Remove' }).click();
-  await expect(page.getByRole('heading', { name: /174 Catalog results/i })).toBeVisible();
+  await page.getByRole('link', { name: 'Remove filter Language: Latin' }).click();
+  await expect(page.getByRole('heading', { name: /174 results/i })).toBeVisible();
   // Expand filters (yet again)
-  await page.getByRole('button', { name: /Options\/Filters/ }).click();
+  await page.getByRole('button', { name: "Show Search Filters" }).click();
+  await page.getByRole('button', { name: "Language" }).click();
   // Japanese is down in the mud
-  await expect(page.getByRole('button', { name: /Japanese/ })).not.toBeVisible();
+  await expect(page.getByRole('link', { name: "Japanese" })).not.toBeVisible();
   // "Show all" to make all language facets visible
   await page.getByRole('button', { name: /Show all \d+ language filters/i }).click();
-  await expect(page.getByRole('button', { name: /Japanese/ })).toBeVisible();
+  await expect(page.getByRole('link', { name: "Japanese" })).toBeVisible();
 });
 
 // Back button and "Clear filters" button remove facets
@@ -54,11 +63,12 @@ test('Clear facets (non-mobile/fullwidth)', async ({ page, isMobile }) => {
   // No filters to be found
   await expect(page.getByRole('heading', { name: 'Current filters' })).not.toBeVisible();
   // Add language filter from the top options
-  await page.getByRole('button', { name: /English/ }).click();
+  await page.getByRole('button', { name: "Language" }).click();
+  await page.getByRole('link', { name: "English" }).click();
   // Now we have filters
   await expect(page.getByRole('heading', { name: 'Current filters' })).toBeVisible();
   // Clear filters
-  await page.getByRole('button', { name: 'Clear filters' }).click();
+  await page.getByRole('link', { name: 'Clear filters' }).click();
   await expect(page.getByRole('heading', { name: 'Current filters' })).not.toBeVisible();
   // Return to filters
   await page.goBack();
@@ -70,20 +80,21 @@ test('Clear facets (non-mobile/fullwidth)', async ({ page, isMobile }) => {
 
 // This is essentially the same test as above but with "Options/Filters" toggled when necessary.
 test('Clear facets (mobile/width-shrink)', async ({ page }) => {
-  page.setViewportSize({ width: 900, height: 1500 });
+  page.setViewportSize({ width: 800, height: 1500 });
   await page.goto('/Search/Home?lookfor=*');
   // No filters to be found
   await expect(page.getByRole('heading', { name: 'Current filters' })).not.toBeVisible();
   // Expand filters
-  await page.getByRole('button', { name: /Options\/Filters/ }).click();
+  await page.getByRole('button', { name: "Show Search Filters" }).click();
+  await page.getByRole('button', { name: "Language" }).click();
   // Add language filter from the top options
-  await page.getByRole('button', { name: /English/ }).click();
+  await page.getByRole('link', { name: "English" }).click();
   // Expand filters (again)
-  await page.getByRole('button', { name: /Options\/Filters/ }).click();
+  await page.getByRole('button', { name: "Show Search Filters" }).click();
   // Now we have filters
   await expect(page.getByRole('heading', { name: 'Current filters' })).toBeVisible();
   // Clear filters
-  await page.getByRole('button', { name: 'Clear filters' }).click();
+  await page.getByRole('link', { name: 'Clear filters' }).click();
   await expect(page.getByRole('heading', { name: 'Current filters' })).not.toBeVisible();
   // Return to filters
   await page.goBack();

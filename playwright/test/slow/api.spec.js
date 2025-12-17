330c8bb3e102fdae63463ad4bc826952d4fd2d3e
Brian "Moses" Hall <moseshll@umich.edu>
2025-12-17 13:26:19 -0500

    Restore test_htid constant dropped when moving to new branch

--- playwright/test/slow/api.spec.js
+++ playwright/test/slow/api.spec.js
@@ -2,6 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 const test_cid = '002312286'; // "Kōkogaku zasshi" arbitrarily chosen
 const test_truncated_cid = '2312286'; // Truncated version
+const test_htid = 'mdp.39015048895836'; // One of the htids on test_cid
 
 test('XML with full CID', async ({ page }) => {
   const response = await page.goto(`/Record/${test_cid}.xml`);
